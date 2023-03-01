b97832910b884139bf1e0947f8c76dca1cdb768b
rav3 <rav3@publicisgroupe.net>
2023-03-01 12:06:12 +0530

    add logger

--- src/main/java/com/javatechie/azure/demo/SpringAzureDemoApplication.java
+++ src/main/java/com/javatechie/azure/demo/SpringAzureDemoApplication.java
@@ -1,16 +1,24 @@
 package com.javatechie.azure.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @SpringBootApplication
 @RestController
 public class SpringAzureDemoApplication {
-
+	Logger logger = LoggerFactory.getLogger(SpringAzureDemoApplication.class);
 	@GetMapping("/message")
 	public String message(){
+		logger.trace("A TRACE Message");
+		logger.debug("A DEBUG Message");
+		logger.info("An INFO Message");
+		logger.warn("A WARN Message");
+		logger.error("An ERROR Message");
 		return "Congrats ! your application deployed successfully in Azure Platform. !";
 	}
 
